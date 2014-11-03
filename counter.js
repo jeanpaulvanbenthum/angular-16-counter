faef474c9f8c8b4dcd6e6f071043aca045d606f3
Adam Merrifield <macagp@gmail.com>
2014-11-03 14:14:18 -0500

    Optimizations.

--- counter.js
+++ counter.js
@@ -2,40 +2,62 @@
   @toc
 
 @param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
-TODO
-
 @param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
-TODO
-
-@dependencies
-TODO
 
 @usage
 partial / html:
-TODO
-
-controller / js:
-TODO
+<div fs-counter value="someValue"
+    data-min="0"
+    data-max="100"
+    data-step="1"
+    data-addclass="someClass"
+    data-width="130px"
+    data-editable
+    ></div>
 
 //end: usage
 */
 
 'use strict';
 
-angular.module('Firestitch.angular-counter', []).directive('fsCounter', [ function () {
+angular.module('Firestitch.angular-counter', []).directive('fsCounter', ['$timeout', function ($timeout) {
 
     return {
         restrict: "A",
         scope: {
             value: "=value"
         },
-        template: "<div class=\"fs-counter input-group\" ng-class=\"addclass\" ng-style=\"width\"><span class=\"input-group-btn\" ng-click=\"minus()\"><button class=\"btn btn-default\"><span class=\"glyphicon glyphicon-minus\"></span></button></span><input type=\"text\" class=\"form-control text-center\" ng-model=\"value\" ng-change=\"changed()\" ng-readonly=\"readonly\"><span class=\"input-group-btn\" ng-click=\"plus()\"><button class=\"btn btn-default\"><span class=\"glyphicon glyphicon-plus\"></span></button></span></div>",
+        template: "<div class=\"fs-counter input-group\" ng-class=\"addclass\" ng-style=\"width\"><span class=\"input-group-btn\" ng-click=\"minus()\"><button class=\"btn btn-default\"><span class=\"glyphicon glyphicon-minus\"></span></button></span><input type=\"text\" class=\"form-control text-center\" ng-model=\"value\" ng-blur=\"blurred()\" ng-change=\"changed()\" ng-readonly=\"readonly\"><span class=\"input-group-btn\" ng-click=\"plus()\"><button class=\"btn btn-default\"><span class=\"glyphicon glyphicon-plus\"></span></button></span></div>",
         replace: true,
-        link: function(scope, element, attributes) {
-            var min = (angular.isUndefined(attributes.min) ? null : parseInt(attributes.min)),
-                max = (angular.isUndefined(attributes.max) ? null : parseInt(attributes.max)),
-                step = (angular.isUndefined(attributes.step) || parseInt(attributes.step) === 0 ? 1 : parseInt(attributes.step)),
-                setValue;
+        link: function(scope, element, attrs) {
+            var min = (angular.isUndefined(attrs.min) ? void 0 : parseInt(attrs.min)),
+                max = (angular.isUndefined(attrs.max) ? void 0 : parseInt(attrs.max)),
+                step = (angular.isUndefined(attrs.step) || parseInt(attrs.step) === 0 ? 1 : parseInt(attrs.step)),
+                setValue,
+                changeDelay;
+
+            /**
+             * Sets the value as an integer. If the value cannot be parsed,
+             * i.e. returns NaN, then the min value or 0 will be used instead.
+             */
+            setValue = function(val) {
+                var parsedVal = parseInt(parsedVal);
+                if (!isNaN(parsedVal)) {
+                    if (min !== undefined && min > parsedVal) {
+                        parsedVal = min;
+                        return parsedVal;
+                    }
+                    if (max !== undefined && max < parsedVal) {
+                        parsedVal = max;
+                        return parsedVal;
+                    }
+                    return parsedVal;
+                } else {
+                    console.log("parsedValue must parse to a number.");
+                    parsedVal = min || 0;
+                    return parsedVal;
+                }
+            };
 
             /**
              * Confirm the value attribute exists on the element
@@ -47,31 +69,15 @@ angular.module('Firestitch.angular-counter', []).directive('fsCounter', [ functi
             /**
              * Set some scope wide properties
              */
-            scope.readonly = (angular.isUndefined(attributes.editable) ? true : false);
-            scope.addclass = (angular.isUndefined(attributes.addclass) ? null : attributes.addclass);
-            scope.width = (angular.isUndefined(attributes.width) ? {} : {width:attributes.width});
-
-            /**
-             * Sets the value as an integer.
-             */
-            setValue = function(val) {
-                var val = parseInt(val);
-                if ((!min && !max) || (min && max && val >= min && val <= max)) {
-                    return val;
-                }
-                val = min && min > val ? min : max && max < val ? max : val;
-                return val;
-            };
+            scope.readonly = (angular.isUndefined(attrs.editable) ? true : false);
+            scope.addclass = (angular.isUndefined(attrs.addclass) ? null : attrs.addclass);
+            scope.width = (angular.isUndefined(attrs.width) ? {} : {width:attrs.width});
             scope.value = setValue(scope.value);
 
             /**
              * Decrement the value and make sure we stay within the limits, if defined.
              */
             scope.minus = function() {
-                if (min && (scope.value <= min || scope.value - step <= min) || min === 0 && scope.value < 1) {
-                    scope.value = setValue(min);
-                    return;
-                }
                 scope.value = setValue(scope.value - step);
             };
 
@@ -79,35 +85,26 @@ angular.module('Firestitch.angular-counter', []).directive('fsCounter', [ functi
              * Increment the value and make sure we stay within the limits, if defined.
              */
             scope.plus = function() {
-                if (max && (scope.value >= max || scope.value + step >= max)) {
-                    scope.value = setValue(max);
-                    return;
-                }
                 scope.value = setValue(scope.value + step);
             };
 
             /**
-              This is only triggered when the field is manually edited by the user.
-              Where we can perform some validation and make sure that they enter the
-              correct values from within the restrictions.
-              */
+             * This is only triggered 1 second after a field is manually edited
+             * by the user. Where we can perform some validation and make sure
+             * that they enter the correct values from within the restrictions.
+             */
             scope.changed = function() {
-                if (!scope.value) {
-                    scope.value = setValue(0);
-                }
-                if (/[0-9]/.test(scope.value)) {
-                    scope.value = setValue(scope.value);
-                } else {
-                    scope.value = setValue(scope.min);
-                }
-                if (min && (scope.value <= min || scope.value - step <= min)) {
-                    scope.value = setValue(min);
-                    return;
-                }
-                if (max && (scope.value >= max || scope.value + step >= max)) {
-                    scope.value = setValue(max);
-                    return;
-                }
+                changeDelay = $timeout(function (){
+                    scope.value =  setValue(scope.value);
+                }, 1000, true);
+            };
+
+            /**
+             * This is only triggered when user leaves a manually edited field.
+             * Where we can perform some validation and make sure that they
+             * enter the correct values from within the restrictions.
+             */
+            scope.blurred = function() {
                 scope.value =  setValue(scope.value);
             };
         }
